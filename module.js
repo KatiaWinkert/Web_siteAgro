dac3a1517cf99611561b40eaf195c8306e4829ac
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-09-26 13:36:11 -0300

    Create Modules.js
    criação de modulos

---
+++ module.js
@@ -0,0 +1,19 @@
+function elementMenu(elem) {
+  if (elem.classList.contains('change')) {
+    elem.innerHTML = '<i class="fas fa-bars fa-2x"></i>'
+  } else {
+    elem.innerHTML = '<i class="fas fa-times fa-2x"></i>'
+  }
+  document.getElementById('element-Menu-Mobile').classList.toggle('active')
+  elem.classList.toggle('change')
+}
+
+
+function elementMenu(elem) {
+  const menuMobile = document.getElementById('element-Menu-Mobile')
+  const elementPioMobile = document.getElementById('element - mobile')
+  elementPioMobile.innerHTML = '<i class="fas fa-bars fa-2x"></i>'
+  elementPioMobile.classList.toggle('change')
+  menuMobile.classList.toggle('change')
+}
+export default elementMenu
