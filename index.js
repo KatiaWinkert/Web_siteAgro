d58f944de1137fd6c71388e4257fa4657da927be
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-09-26 13:35:50 -0300

    Update index.js
    atualização 13:35

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import { format, compareAsc } from 'date-fns'
+import elementMenu from './module.js'
+const result = elementMenu()
+console.log(result)
 
-const formatDate = format(new Date(), 'dd/MM/yyyy')
-//=> '02/11/2014'
-console.log(formatDate)
-const dates = [
-  new Date(1995, 6, 2),
-  new Date(1987, 1, 11),
-  new Date(1989, 6, 10),
-]
-dates.sort(compareAsc)
-//=> [
-//   Wed Feb 11 1987 00:00:00,
-//   Mon Jul 10 1989 00:00:00,
-//   Sun Jul 02 1995 00:00:00
-// ]
+window.onload = () => {
+  const contatoDoform = document.querySelector('#ctt-form')
+  contatoDoform.addEventListener('submit', onsubmit)
+}
+
+function onsubmit(event) {
+  event.preventDefault()
+  const nome = document.querySelector('#nome').value
+  const email = document.querySelector('#email').value
+  const telefone = document.querySelector('#telefone').value
+  const mensagem = document.querySelector('#mensagem').value
+}
